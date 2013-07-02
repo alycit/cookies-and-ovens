9ca375b854243d9a1768a6f3386531e5256ba7ab
dbc-apprentice <apprentice@devbootcamp.com>
2013-07-02 13:39:24 -0500

    cookie spec

--- jasmine/spec/CookieSpec.js
+++ jasmine/spec/CookieSpec.js
@@ -1,2 +1,24 @@
 describe("Cookie", function() {
+  var cookie;
+  var cookieType = "chocolate chip";
+
+  beforeEach(function() {
+    cookie = new Cookie(10, cookieType);
+  });
+
+  describe("#getCookieType", function() {
+    it("should return the cookie type", function(){
+      expect(cookie.getCookieType()).toEqual("chocolate chip");
+    });
+  });
+
+  describe("#constructor", function() {
+    it("require that bakeTime argument is numeric", function() {
+      expect(function() { new Cookie("foo", cookieType)}).toThrow("bakeTime must be numeric");
+    });
+
+    it("requires two arguments", function() {
+      expect(function() { new Cookie(10)}).toThrow("Invalid number of args");
+    });
+  });
 });
